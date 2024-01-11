7bc85f0dce4d2749a04d562197b721191f4459ea
Ian Kisali <iankisali@gmail.com>
2024-01-11 16:10:15 +0300

    update default tf file

--- aws-automate-infrastructure/default-main.tf
+++ aws-automate-infrastructure/default-main.tf
@@ -1,3 +1,38 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+variable "vpc_cidr_blocks" {}
+variable "subnet_cidr_blocks" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_blocks
+    tags = {
+      Name: "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_blocks
+    availability_zone = var.avail_zone
+    tags = {
+      Name: "${var.env_prefix}-subnet-1"
+    }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name: "${var.env_prefix}-internetgateway"
+    }
+}
+
 resource "aws_default_route_table" "main-rtb" {
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 
